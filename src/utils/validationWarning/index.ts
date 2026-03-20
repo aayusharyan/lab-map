8dc7e19ad5291c64abdbf19f344779818504a549
Aayush Sinha <aayush.aryan@me.com>
2026-03-21 00:49:08 +0900

    Add barrel export for validation warning utils

---
+++ src/utils/validationWarning/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @file index.ts
+ * @description Barrel export for validation warning utilities
+ *
+ * Re-exports public API for the validation warning utility module:
+ * - getValidationWarningHash: Stable hash helper for dismissal tracking
+ * - Types: ValidationWarning
+ */
+
+export { getValidationWarningHash } from './validationWarning';
+export type { ValidationWarning } from './validationWarning.types';
