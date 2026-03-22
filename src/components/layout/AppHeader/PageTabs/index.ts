091e0c2c1a5b66b1876845dd4336648ce47e161e
Aayush Sinha <aayush.aryan@me.com>
2026-03-22 19:36:26 +0900

    Add Barrel export for page tabs

---
+++ src/components/layout/AppHeader/PageTabs/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @file index.ts
+ * @description Barrel export for the PageTabs module
+ *
+ * Public API:
+ * - Components:
+ *   - PageTabs
+ *
+ * Import style:
+ * import { PageTabs } from '@/components/layout/AppHeader/PageTabs';
+ */
+
+export { PageTabs } from './PageTabs';
