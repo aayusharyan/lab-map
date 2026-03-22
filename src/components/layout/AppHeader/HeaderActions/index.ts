ee54299965317ebc5dd84af495aed867059a3ebe
Aayush Sinha <aayush.aryan@me.com>
2026-03-22 21:07:08 +0900

    Add Barrel export for header action

---
+++ src/components/layout/AppHeader/HeaderActions/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @file index.ts
+ * @description Barrel export for the HeaderActions module
+ *
+ * Public API:
+ * - Components:
+ *   - HeaderActions
+ *
+ * Import style:
+ * import { HeaderActions } from '@/components/layout/AppHeader/HeaderActions';
+ */
+
+export { HeaderActions } from './HeaderActions';
