8e7dfa64d73e709e1f4a1eb33969dd23ff338642
Aayush Sinha <aayush.aryan@me.com>
2026-03-23 03:27:00 +0900

    Add barrel export for settings

---
+++ src/components/layout/SettingsPanel/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @file index.ts
+ * @description Barrel export for the SettingsPanel module
+ *
+ * Public API:
+ * - Components:
+ *   - SettingsPanel
+ *
+ * Import style:
+ * import { SettingsPanel } from '@/components/layout/SettingsPanel';
+ */
+
+export { SettingsPanel } from './SettingsPanel';
