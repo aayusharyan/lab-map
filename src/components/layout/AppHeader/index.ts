f22bbc9342d45295525fe8d1cc3278ab64c4c7e6
Aayush Sinha <aayush.aryan@me.com>
2026-03-22 19:38:44 +0900

    Bugfix comment on the App Header export

--- src/components/layout/AppHeader/index.ts
+++ src/components/layout/AppHeader/index.ts
@@ -1,6 +1,13 @@
 /**
  * @file index.ts
- * @description Barrel export for AppHeader component
+ * @description Barrel export for the AppHeader module
+ *
+ * Public API:
+ * - Components:
+ *   - AppHeader
+ *
+ * Import style:
+ * import { AppHeader } from '@/components/layout/AppHeader';
  */
 
 export { AppHeader } from './AppHeader';
