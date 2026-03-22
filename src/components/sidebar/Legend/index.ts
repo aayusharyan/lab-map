3de5a78d221565709d1d2d227024bd8241254842
Aayush Sinha <aayush.aryan@me.com>
2026-03-23 03:29:22 +0900

    Update comment documentation for Legend module

--- src/components/sidebar/Legend/index.ts
+++ src/components/sidebar/Legend/index.ts
@@ -1,6 +1,13 @@
 /**
  * @file index.ts
- * @description Barrel export for Legend component
+ * @description Barrel export for the Legend module
+ *
+ * Public API:
+ * - Components:
+ *   - Legend
+ *
+ * Import style:
+ * import { Legend } from '@/components/sidebar/Legend';
  */
 
 export { Legend } from './Legend';
