e31b565906c417bbe9ac1fe00c22f9960623614d
Aayush Sinha <aayush.aryan@me.com>
2026-04-07 06:23:18 +0900

    Add Barrel export for Notification item

---
+++ src/components/NotificationStack/NotificationItem/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @file index.ts
+ * @description Barrel export for NotificationItem component
+ */
+
+export { NotificationItem } from './NotificationItem';
+export type { NotificationItemProps } from './NotificationItem.types';
