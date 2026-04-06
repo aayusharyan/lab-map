433207a0d6490f06b6d202ecc8366b0b19922440
Aayush Sinha <aayush.aryan@me.com>
2026-04-07 06:49:35 +0900

    Add new exports for settings context

--- src/context/SettingsContext/index.ts
+++ src/context/SettingsContext/index.ts
@@ -2,12 +2,24 @@
  * @file index.ts
  * @description Barrel export for SettingsContext module
  *
- * Re-exports public API for the SettingsContext module:
- * - SettingsProvider: Provider component for wrapping the app
- * - SettingsContext: React context instance (for hooks)
- * - Types: SettingsState, SettingsAction
+ * Public API:
+ * - Context:
+ *   - SettingsProvider
+ *   - SettingsContext
+ * - Types:
+ *   - AppSettings, SettingsState, SettingsAction
+ *   - FontSize (re-exported from @/types/font)
+ *   - ScrollBehavior (re-exported from @/types/scroll)
+ * - Constants:
+ *   - FONT_SIZES (re-exported from @/types/font)
+ *
+ * Import style:
+ * import { SettingsProvider, type AppSettings } from '@/context/SettingsContext';
  */
 
 export { SettingsProvider } from './SettingsContext';
 export { SettingsContext } from './SettingsContext.types';
 export type { AppSettings, SettingsState, SettingsAction } from './SettingsContext.types';
+export type { FontSize } from '@/types/font';
+export type { ScrollBehavior } from '@/types/scroll';
+export { FONT_SIZES } from '@/types/font';
