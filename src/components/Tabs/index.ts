435933f89f2f44ce1a2379cc776fe8d6f9d1e594
Aayush Sinha <aayush.aryan@me.com>
2026-04-08 08:53:23 +0900

    Update comments

--- src/components/Tabs/index.ts
+++ src/components/Tabs/index.ts
@@ -1,6 +1,18 @@
 /**
  * @file index.ts
- * @description Barrel export for Tabs component
+ * @description Barrel export for Tabs
+ *
+ * Public API:
+ * - Tabs: tab-strip component supporting multiple visual variants
+ * - type TabItem: data shape for a single tab entry
+ * - type TabsProps: props for the Tabs component
+ * - type TabsVariant: union of supported visual variant identifiers
+ *
+ * @see Tabs.tsx - Component implementation
+ * @see Tabs.types.ts - Type contracts
+ *
+ * Import style:
+ * import { Tabs, type TabItem, type TabsProps, type TabsVariant } from '@/components/Tabs';
  */
 
 export { Tabs } from './Tabs';
