e32b1c395d9733e39c65370f8c4fe5f7ef5507e1
Aayush Sinha <aayush.aryan@me.com>
2026-04-14 21:27:41 +0900

    Add Barrel export for footer

---
+++ src/components/footer/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @file index.ts
+ * @description Barrel export for Footer component
+ *
+ * Import style:
+ * import { Footer } from '@/components/footer';
+ */
+
+export { Footer } from './Footer';
+export type { FooterProps } from './Footer.types';
